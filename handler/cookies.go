238a925c9e0f85d34e0455e38daf48b1b79f7582
Haotian Zeng <23140824+A-440Hz@users.noreply.github.com>
2025-04-07 16:37:07 -0700

    user cookie

--- handler/cookies.go
+++ handler/cookies.go
@@ -1,23 +1,35 @@
 package handler
 
 import (
-	"fmt"
 	"job/internal/db"
 	"net/http"
+)
+
+const (
+	userCookie                    = "userCookie"
+	defaultUnregisteredUserExpiry = 3600 * 24 * 7
 )
 
 // todo: probably incorporate encryption to get/set cookies
 // https://www.alexedwards.net/blog/working-with-cookies-in-go
 func getUserCookie(w http.ResponseWriter, r *http.Request) (string, error) {
-	cookie, err := r.Cookie("userCookie")
+	cookie, err := r.Cookie(userCookie)
 	if err != nil {
 		return "", err
 	}
 	return cookie.Value, nil
 }
 
-func setUserCookie() error {
+func setUserCookie(w http.ResponseWriter, r *http.Request) error {
+	// probably separate this logic to better handle unregistered users
 	uuid := db.NewID(db.UserIdPrefix)
-	fmt.Println(uuid)
+	cookie := http.Cookie{
+		Name:     userCookie,
+		Value:    uuid,
+		MaxAge:   defaultUnregisteredUserExpiry,
+		Secure:   true,
+		SameSite: http.SameSiteStrictMode,
+	}
+	http.SetCookie(w, &cookie)
 	return nil
 }
