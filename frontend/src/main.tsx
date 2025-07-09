d6ef5f23825b8937b235c8bf4c11b1ae8601bb05
Haotian Zeng <23140824+A-440Hz@users.noreply.github.com>
2025-07-08 18:30:42 -0700

    initial offering

---
+++ frontend/src/main.tsx
@@ -0,0 +1,12 @@
+// this file bootstraps the React app and connects it to the HTML page
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
