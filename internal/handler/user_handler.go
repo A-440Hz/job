81769d3afdd9a18482e66170786290cd72e39924
Haotian <23140824+A-440Hz@users.noreply.github.com>
2025-10-27 16:23:23 -0700

    fix user registration error

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -79,6 +79,7 @@ func (h *Handler) RegisterBaseUser(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(true)
 	// do i return this? idk what react needs yet
 	// json.NewEncoder(w).Encode(u)
 }
