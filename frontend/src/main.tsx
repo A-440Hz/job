9b355b53f4d9e09ba5198f7ae22901558d8fe65f
Haotian <23140824+A-440Hz@users.noreply.github.com>
2025-10-27 19:43:25 -0700

    scope?

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,7 +11,7 @@ import './index.css'
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('./assets/sw.js')
+      .register('./assets/sw.js', { scope: './' })
       .then((registration) => {
         console.log('[App] Service Worker registered successfully:', registration.scope);
       })
