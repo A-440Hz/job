f12cbd68d02840ceb8eae1635fad1d8079240a3b
Haotian <23140824+A-440Hz@users.noreply.github.com>
2025-10-27 17:58:21 -0700

    allowed hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   publicDir: './src/assets',
   base: './',
   server: {
-    allowedHosts: ['.localhost', "frontend-staging-60b5.up.railway.app"],
+    allowedHosts: ['.localhost', "frontend-staging-60b5.up.railway.app", "jobapptracker.up.railway.app", "haotianswebsite.com"],
     host: '0.0.0.0',
   },
   build: {
