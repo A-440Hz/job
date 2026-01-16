1e8f8f8fe18198f95c19850ca8b315715ffd4d2d
Haotian Zeng <23140824+A-440Hz@users.noreply.github.com>
2026-01-15 19:58:46 -0800

    nit: everyone sees the loading page spin

--- frontend/src/LoadingSpin.tsx
+++ frontend/src/LoadingSpin.tsx
@@ -5,7 +5,7 @@
 function LoadingSpin() {  
     return (
         <img 
-            className="w-6 mr-1 rounded-xl motion-reduce:animate-[spin_.6s_linear_infinite]"
+            className="w-6 mr-1 rounded-xl motion-safe:animate-[spin_.6s_linear_infinite]"
             src="/squid-panic.png" 
             alt="Loading..."
         />
